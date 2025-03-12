47434ec225c3c85ee6cb3529ce9b2c8140e24fd6
Akeka_ <vladvaledol@gmail.com>
2025-03-12 21:18:58 +0300

    UserContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
+import { UserProvider } from './context/UserContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <UserProvider><App /></UserProvider>
 )
